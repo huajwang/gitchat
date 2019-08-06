cdeaf6c46b8c38ca8574088f78e89c54fda1e2d1
huajwang <cse63152@gmail.com>
2019-08-06 20:26:39 +0800

    use GetMapping instead

--- async-message/vendor-service/src/main/java/com/longmaple/ttmall/vendorsvr/web/VendorController.java
+++ async-message/vendor-service/src/main/java/com/longmaple/ttmall/vendorsvr/web/VendorController.java
@@ -10,17 +10,18 @@ import com.longmaple.ttmall.vendorsvr.model.Vendor;
 import com.longmaple.ttmall.vendorsvr.service.VendorService;
 
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
-@RequestMapping(value="vendors")
+@RequestMapping(value="/vendors")
 public class VendorController {
 	
     @Autowired
     private VendorService vendorService;
 
-    @RequestMapping(value="/{vendorId}", method = RequestMethod.GET)
+    @GetMapping("/{vendorId}")
     public Vendor getVendor( @PathVariable("vendorId") String vendorId) {
         return vendorService.getVendor(vendorId);
     }
