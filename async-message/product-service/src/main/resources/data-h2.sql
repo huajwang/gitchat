182f832eeeb8e34254c319d8f314192876ccbc47
Huajian Wang <cse63152@gmail.com>
2019-08-07 00:34:52 -0400

    update uuid;

--- async-message/product-service/src/main/resources/data-h2.sql
+++ async-message/product-service/src/main/resources/data-h2.sql
@@ -1,8 +1,8 @@
 INSERT INTO product(product_id, vendor_id, category, product_name, in_stock, price)
-VALUES ('f3831f8c-c338-4ebe-a82a-e2fc1d1ff78a', 'e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'food','新疆哈密瓜', 300, 8.5);
+VALUES ('5c9c5b3d-f7f3-411b-8134-09fdd6333d82', '6c06e70b-138a-4f17-9298-ad32d75b0de5', 'food','新疆哈密瓜', 300, 8.5);
 INSERT INTO product(product_id, vendor_id, category, product_name, in_stock, price)
-VALUES ('t9876f8c-c338-4abc-zf6a-ttt1', 'e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'cigar','宇宙牌香烟', 208, 19.9);
+VALUES ('96bfca0e-185e-4b9d-8990-4de97ee9c24f', '6c06e70b-138a-4f17-9298-ad32d75b0de5', 'cigar','宇宙牌香烟', 208, 19.9);
 INSERT INTO product(product_id, vendor_id, category, product_name, in_stock, price)
-VALUES ('38777179-7094-4200-9d61-edb101c6ea84', '442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 'drink','牛栏山老白干', 300, 198.52);
+VALUES ('160694bd-5006-4109-be86-9c80636e9fd7', '50219a94-f12e-4769-852f-2acb12aca418', 'drink','牛栏山老白干', 300, 198.52);
 INSERT INTO product(product_id, vendor_id, category, product_name, in_stock, price)
-VALUES ('08dbe05-606e-4dad-9d33-90ef10e334f9', '442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 'tea','安溪铁观音', 136, 298.99);
+VALUES ('cbe7dd34-7312-46a8-8942-99af716a0aea', '50219a94-f12e-4769-852f-2acb12aca418', 'tea','安溪铁观音', 136, 298.99);
