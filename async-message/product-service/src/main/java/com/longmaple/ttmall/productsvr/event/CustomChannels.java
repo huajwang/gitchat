e00b8f6dc46a183f7910ff9a3c906bb6b18965af
huajwang <cse63152@gmail.com>
2019-08-07 11:28:23 +0800

    change method name;

--- async-message/product-service/src/main/java/com/longmaple/ttmall/productsvr/event/CustomChannels.java
+++ async-message/product-service/src/main/java/com/longmaple/ttmall/productsvr/event/CustomChannels.java
@@ -5,5 +5,5 @@ import org.springframework.messaging.SubscribableChannel;
 
 public interface CustomChannels {
 	@Input("inboundVendorChanges")
-	SubscribableChannel orgs();
+	SubscribableChannel vendors();
 }
